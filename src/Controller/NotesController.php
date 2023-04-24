b20e09bba671c897f30d8efacc025bc67328eab7
gBoole01 <68549849+gBoole01@users.noreply.github.com>
2023-04-24 10:48:47 +0200

    feat: add Note Controller

---
+++ src/Controller/NotesController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class NotesController extends AbstractController
+{
+    #[Route('/notes', name: 'app_notes')]
+    public function index(): Response
+    {
+        return $this->render('notes/index.html.twig', [
+            'controller_name' => 'NotesController',
+        ]);
+    }
+}
